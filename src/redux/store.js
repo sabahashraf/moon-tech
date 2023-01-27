856694b575cfb087fe29bd89f1dc827153588f6f
sabahashraf <sabahsabrina@yahoo.com>
2023-01-27 13:57:21 -0800

    redux-devtool integrate

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,7 @@
 import { createStore } from "redux";
 import productreducer from "./reducer/productReducer";
+import { composeWithDevTools } from "@redux-devtools/extension";
 
-const store = createStore(productreducer);
+const store = createStore(composeWithDevTools(), productreducer);
 
 export default store;
